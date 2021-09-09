fdc4a18ba2818c6f7574dbcef3dcc2f08d564e5d
BurrPro <77427249+BurrPro@users.noreply.github.com>
2021-09-10 00:17:58 +1200

    Updated values to align with name of game

--- game.py
+++ game.py
@@ -1,15 +1,15 @@
 import random
-answer = random.randint(1, 100)
-guess = int(input("Guess a number between 1 and 100: "))
+answer = random.randint(1, 1000)
+guess = int(input("Guess a number between 1 and 1000: "))
 tries = 0
 
-while guess != answer and tries < 10:
+while guess != answer and tries < 20:
     if guess < answer:
         print("Try again, the correct number is higher.")
-        guess = int(input("Guess a number between 1 and 100: "))
+        guess = int(input("Guess a number between 1 and 1000: "))
     elif guess > answer:
         print("Try again, the correct number is lower.")
-        guess = int(input("Guess a number between 1 and 100:"))
+        guess = int(input("Guess a number between 1 and 1000:"))
 
 print(
     "You guessed the number correctly! The correct number was %s, you guessed this in %d tries!"
